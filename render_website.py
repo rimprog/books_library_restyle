0848ccd343fde5fbeab680d1eb32587ebd5bb462
rimprog <riminprog@gmail.com>
2021-08-20 19:42:59 +0300

    Split books by two columns.

--- render_website.py
+++ render_website.py
@@ -2,13 +2,14 @@ import os
 import json
 from functools import partial
 
+from more_itertools import chunked
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def on_reload(env, books_description):
+def on_reload(env, books_description_chunks):
     template = env.get_template('template.html')
-    rendered_page = template.render(books_description=books_description)
+    rendered_page = template.render(books_description_chunks=books_description_chunks)
 
     with open('index.html', 'w', encoding='utf-8') as file:
         file.write(rendered_page)
@@ -20,12 +21,14 @@ def main():
     with open(books_description_path, 'r') as my_file:
         books_description = json.load(my_file)
 
+    books_description_chunks = list(chunked(books_description, 2))
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    on_reload_with_args = partial(on_reload, env, books_description)
+    on_reload_with_args = partial(on_reload, env, books_description_chunks)
     on_reload_with_args()
 
     server = Server()
